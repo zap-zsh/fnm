79187c40e44d4680890044b57f2be1c0c4ed1efc
Dorian Li <49279922+dorian-li@users.noreply.github.com>
2022-12-04 00:37:49 +0800

    feat: apply fnm completion on zsh

--- fnm.plugin.zsh
+++ fnm.plugin.zsh
@@ -4,4 +4,5 @@
 eval "$(fnm env --use-on-cd)"
 
 # Add fnm completions
-fnm completions --shell zsh
+fnm_completion="$(dirname "$(readlink -f "$0")")/_fnm"
+fnm completions --shell zsh > "$fnm_completion"
